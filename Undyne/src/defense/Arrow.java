e564b087e913425a744e0e20ad81106afbea1509
akandikatla <aaron4game@gmail.com>
2017-04-27 09:39:48 -0500

    Revert "Moved runner methods to Player and Attack class"
    This reverts commit b66dccd11a350cdbfabcef4c7e7a76636974105c.

--- Undyne/src/defense/Arrow.java
+++ Undyne/src/defense/Arrow.java
@@ -11,128 +11,130 @@ import java.io.IOException;
 
 import javax.imageio.ImageIO;
 
+
 /*
  * Composes an arrow to be implemented in the Attack class
  */
 public class Arrow {
-	/*
-	 * This determines the speed the arrow should go at between 1 – 100
-	 */
-	private int speed;
-	/*
-	 * This determines whether or not the arrow will be reversed
-	 */
-	private boolean reverse;
-	/*
-	 * This determines the direction the arrow should come from
-	 */
-	private char direction;
-	/*
-	 * These are the coordinates of the arrow
-	 */
-	private int x, y;
-	/*
-	 * Color of the arrow
-	 */
-	private Color arrColor;
-
-	public Arrow(int speed, boolean reverse, char direction) {
-		this.speed = speed;
-		this.reverse = reverse;
-		this.direction = direction;
-		setCoordinates(direction);
-	}
-
-	/*
-	 * Helper method for the constructor setting the arrow coordinates
-	 */
-	private void setCoordinates(char direction) {
-
-		switch (direction) {
-		case 'r':
-			x = 11;
-			y = 270;
-			break;
-		case 'l':
-			x = 557;
-			y = 270;
-			break;
-		case 'u':
-			x = 285;
-			y = 545;
-			break;
-		case 'd':
-			x = 285;
-			y = 0;
-			break;
-		}
-	}
-
-	public void tick() {
-		switch (direction) {
-		case 'l':
-			x -= speed;
-			break;
-		case 'r':
-			x += speed;
-			break;
-		case 'u':
-			y -= speed;
-			break;
-		case 'd':
-			y += speed;
-			break;
-		}
-	}
-
-	public void draw(Graphics g, Color c) throws IOException {
-		BufferedImage arr = ImageIO.read(new File("arrowB.png"));
-		if (c.equals(Color.RED)) {
-			try {
-				arr = ImageIO.read(new File("arrowR.png"));
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
-		if (reverse) {
-			try {
-				arr = ImageIO.read(new File("arrowRE.png"));
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
-		int angle = 0;
-		switch (direction) {
-		case 'r':
-			angle = 0;
-			break;
-		case 'd':
-			angle = 90;
-			break;
-		case 'l':
-			angle = 180;
-			break;
-		case 'u':
-			angle = -90;
-			break;
-		}
-		AffineTransform tx = new AffineTransform();
-		tx.rotate(Math.toRadians(angle), arr.getMinX() + arr.getWidth() / 2, arr.getMinY() + arr.getHeight() / 2);
-		AffineTransformOp op = new AffineTransformOp(tx, AffineTransformOp.TYPE_BILINEAR);
-		arr = op.filter(arr, null);
-		g.drawImage(arr, x, y, null);
-	}
-
-	public int getX() {
-		return x;
-	}
-
-	public int getY() {
-		return y;
-	}
-
-	public char getDir() {
-		return direction;
-	}
-
+    /*
+     * This determines the speed the arrow should go at between 1 – 100
+     */
+    private int speed;
+    /*
+     * This determines whether or not the arrow will be reversed
+     */
+    private boolean reverse;
+    /*
+     * This determines the direction the arrow should come from
+     */
+    private char direction;
+    /*
+     * These are the coordinates of the arrow
+     */
+    private int x, y;
+    /*
+     * Color of the arrow
+     */
+    private Color arrColor;
+    
+    public Arrow(int speed, boolean reverse, char direction) {
+        this.speed = speed;
+        this.reverse = reverse;
+        this.direction = direction;
+        setCoordinates(direction);
+    }
+    
+    /*
+     * Helper method for the constructor setting the arrow coordinates
+     */
+    private void setCoordinates(char direction) {
+        switch(direction) {
+            case 'r':
+                x = 0;
+                y = 270;
+                break;
+            case 'l':
+                x = 590;
+                y = 270;
+                break;
+            case 'u':
+                x = 285;
+                y = 590;
+                break;
+            case 'd':
+                x = 285;
+                y = 0;
+                break;
+        }
+    }
+    
+    public void tick() {
+        switch(direction) {
+            case 'l':
+                x -= speed;
+                break;
+            case 'r':
+                x += speed;
+                break;
+            case 'u':
+                y -= speed;
+                break;
+            case 'd':
+                y += speed;
+                break;
+        }
+    }
+    
+    public void draw(Graphics g, Color c) throws IOException {
+        BufferedImage arr = ImageIO.read(new File("arrowB.png"));
+        if(c.equals(Color.RED)) {
+            try {
+                arr = ImageIO.read(new File("arrowR.png"));
+            }
+            catch(IOException e) {
+                e.printStackTrace();
+            }
+        }
+        if(reverse) {
+            try {
+                arr = ImageIO.read(new File("arrowRE.png"));
+            }
+            catch(IOException e) {
+                e.printStackTrace();
+            }
+        }
+        int angle = 0;
+        switch(direction) {
+            case 'r':
+                angle = 0;
+                break;
+            case 'd':
+                angle = 90;
+                break;
+            case 'l':
+                angle = 180;
+                break;
+            case 'u':
+                angle = -90;
+                break;
+        }
+        AffineTransform tx = new AffineTransform();
+        tx.rotate(Math.toRadians(angle), arr.getMinX() + arr.getWidth()/2, arr.getMinY() + arr.getHeight()/2);
+        AffineTransformOp op = new AffineTransformOp(tx, AffineTransformOp.TYPE_BILINEAR);
+        arr = op.filter(arr, null);
+        g.drawImage(arr, x, y, null);
+    }
+    
+    public int getX() {
+        return x;
+    }
+    
+    public int getY() {
+        return y;
+    }
+    
+    public char getDir() {
+        return direction;
+    }
+    
 }
--- Undyne/src/defense/Attack.java
+++ Undyne/src/defense/Attack.java
@@ -5,160 +5,126 @@ import java.awt.Graphics;
 import java.io.IOException;
 import java.util.LinkedList;
 
-import javax.sound.sampled.UnsupportedAudioFileException;
-
-import nikunj.classes.Sound;
 
 public class Attack {
-	/*
-	 * List of Arrow objects that make up one attack
-	 */
-	private LinkedList<Arrow> attackPattern = new LinkedList<Arrow>();
-	/*
-	 * ifthe delay for an attack is constant, this is it (milliseconds)
-	 */
-	private double delay;
-	/*
-	 * ifthe delays vary in amount for the arrows, this is it (milliseconds)
-	 */
-	private double[] delayGroup;
-	private int counter = 0;
-	public static final char[] DIRS = { 'u', 'd', 'r', 'l' };
-	public static int currentDirection = 0;
-
-	public static String hit = "";
-
-
-	/*
-	 * Constructor for constant delay
-	 */
-	public Attack(LinkedList<Arrow> attackPattern, double delay) {
-		this.attackPattern = attackPattern;
-		this.delay = delay;
-		this.delayGroup = new double[0];
-	}
-
-	/*
-	 * Constructor for varying delays
-	 */
-	public Attack(LinkedList<Arrow> attackPattern, double[] delayGroup) throws IOException {
-		if (delayGroup.length == attackPattern.size() - 1)
-			throw new IOException("Error: Number of delays must be one less than the number of attacks");
-		this.attackPattern = attackPattern;
-		this.delay = 0;
-		this.delayGroup = delayGroup;
-	}
-
-	public void tick() {
-		for (Arrow a : attackPattern)
-			a.tick();
-	}
-
-	public void addArrow(Arrow a) {
-		attackPattern.add(a);
-	}
-
-	public void removeArrow(Arrow a) {
-		attackPattern.remove(a);
-	}
-
-	public String removeArrow(char dir) {
-		boolean hit = false;
-		boolean damage = false;
-		if (attackPattern.size() == 0)
-			return "";
-		for (int i = 0; i < attackPattern.size(); i++) {
-			Arrow tempArrow = attackPattern.get(i);
-			switch (tempArrow.getDir()) {
-
-			case 'l':
-				if (dir == 'r') {
-					if (tempArrow.getX() < 325) {
-						attackPattern.remove(i);
-						hit = true;
-					}
-				} else if (attackPattern.get(i).getX() < 308) {
-					attackPattern.remove(i);
-					damage = true;
-				}
-
-				break;
-			case 'r':
-				if (dir == 'l') {
-					if (tempArrow.getX() > 240) {
-						attackPattern.remove(i);
-						hit = true;
-					}
-				} else if (attackPattern.get(i).getX() > 262) {
-					attackPattern.remove(i);
-					damage = true;
-				}
-
-				break;
-			case 'u':
-				if (dir == 'd') {
-					if (tempArrow.getY() < 320) {
-						attackPattern.remove(i);
-						hit = true;
-					}
-				} else if (attackPattern.get(i).getY() < 295) {
-					attackPattern.remove(i);
-					damage = true;
-				}
-
-				break;
-			case 'd':
-				if (dir == 'u') {
-					if (attackPattern.get(i).getY() > 230) {
-						attackPattern.remove(i);
-						hit = true;
-					}
-				} else if (attackPattern.get(i).getY() > 252) {
-					attackPattern.remove(i);
-					damage = true;
-				}
-				break;
-			}
-		}
-		if (hit)
-			return "H";
-		else if (damage)
-			return "D";
-		return "";
-	}
-
-	public void draw(Graphics g) throws IOException {
-		if (attackPattern.size() == 0)
-			return;
-		attackPattern.get(0).draw(g, Color.RED);
-		for (int i = 1; i < attackPattern.size(); ++i) {
-			attackPattern.get(i).draw(g, Color.BLUE);
-		}
-	}
-
-	public void spawnArrows(Graphics g, Player p) throws IOException {
-		tick();
-		if (++counter == 35) {
-			addArrow(new Arrow(2, false, DIRS[currentDirection++]));
-			if (currentDirection == DIRS.length)
-				currentDirection = 0;
-			counter = 0;
-		}
-		hit = removeArrow(p.getDir());
-		Sound block = null;
-		Sound damage = null;
-		try {
-			block = new Sound("block.wav", false);
-			damage = new Sound("damage.wav", false);
-		} catch (IOException | UnsupportedAudioFileException e) {
-			e.printStackTrace();
-		}
-		if (hit.equals("H")) {
-			p.setRed(0);
-			block.play();
-		} else if (hit.equals("D"))
-			damage.play();
-		draw(g);
-	}
-	
+    /*
+     * List of Arrow objects that make up one attack
+     */
+    private LinkedList<Arrow> attackPattern = new LinkedList<Arrow>();
+    /*
+     * ifthe delay for an attack is constant, this is it (milliseconds)
+     */
+    private double delay;
+    /*
+     * ifthe delays vary in amount for the arrows, this is it (milliseconds)
+     */
+    private double[] delayGroup;
+    
+    /*
+     * Constructor for constant delay
+     */
+    public Attack(LinkedList<Arrow> attackPattern, double delay) {
+        this.attackPattern = attackPattern;
+        this.delay = delay;
+        this.delayGroup = new double[0];
+    }
+    
+    /*
+     * Constructor for varying delays
+     */
+    public Attack(LinkedList<Arrow> attackPattern, double[] delayGroup) throws IOException {
+        if(delayGroup.length == attackPattern.size() - 1)
+            throw new IOException("Error: Number of delays must be one less than the number of attacks");
+        this.attackPattern = attackPattern;
+        this.delay = 0;
+        this.delayGroup = delayGroup;
+    }
+    
+    public void tick() {
+        for(Arrow a : attackPattern)
+            a.tick();
+    }
+    
+    public void addArrow(Arrow a) {
+        attackPattern.add(a);
+    }
+    
+    public void removeArrow(Arrow a) {
+        attackPattern.remove(a);
+    }
+    
+    public String removeArrow(char dir) {
+        boolean hit = false;
+        boolean damage = false;
+        if(attackPattern.size() == 0)
+            return "";
+        for (int i = 0; i < attackPattern.size(); i++) {
+            Arrow tempArrow = attackPattern.get(i);
+            switch(tempArrow.getDir()) {
+                case 'l':
+                    if(dir == 'r') {
+                        if(tempArrow.getX() < 325) {
+                            attackPattern.remove(i);
+                            hit = true;
+                        }
+                    }
+                    else if(attackPattern.get(i).getX() < 295) {
+                        attackPattern.remove(i);
+                        damage = true;
+                    }
+                    break;
+                case 'r':
+                    if(dir == 'l') {
+                        if(tempArrow.getX() > 240) {
+                            attackPattern.remove(i);
+                            hit = true;
+                        }
+                    }
+                    else if(attackPattern.get(i).getX() > 280) {
+                        attackPattern.remove(i);
+                        damage = true;
+                    }
+                    break;
+                case 'u':
+                    if(dir == 'd') {
+                        if(tempArrow.getY() < 320) {
+                            attackPattern.remove(i);
+                            hit = true;
+                        }
+                    }
+                    else if(attackPattern.get(i).getY() < 280) {
+                        attackPattern.remove(i);
+                        damage = true;
+                    }
+                    break;
+                case 'd':
+                    if(dir == 'u') {
+                        if(attackPattern.get(i).getY() > 230) {
+                            attackPattern.remove(i);
+                            hit = true;
+                        }
+                    }
+                    else if(attackPattern.get(i).getY() > 260) {
+                        attackPattern.remove(i);
+                        damage = true;
+                    }
+                    break;
+            }
+        }
+        if(hit)
+            return "H";
+        else if(damage)
+            return "D";
+        return "";
+    }
+    
+    public void draw(Graphics g) throws IOException {
+        if(attackPattern.size() == 0)
+            return;
+        attackPattern.get(0).draw(g, Color.RED);
+        for(int i = 1; i < attackPattern.size(); ++i) {
+            attackPattern.get(i).draw(g, Color.BLUE);
+        }
+    }
+    
 }
