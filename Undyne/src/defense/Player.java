0d4f7b785951598be9db0e324e8603accee16262
akandikatla <aaron4game@gmail.com>
2017-04-27 08:46:38 -0500

    Revert "Moved runner methods to Player and Attack class"
    This reverts commit b66dccd11a350cdbfabcef4c7e7a76636974105c.

--- Undyne/src/defense/Player.java
+++ Undyne/src/defense/Player.java
@@ -1,114 +1,39 @@
 package defense;
 
-import java.awt.Graphics;
-import java.awt.geom.AffineTransform;
-import java.awt.image.AffineTransformOp;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
-
-import javax.imageio.ImageIO;
-
 public class Player {
-	
-	/*
-	 * Initial player health
-	 */
-	private int playerHealth = 100;
-	/*
-	 * How much the player is currently rotated by
-	 */
-	private int angle = 0;
-	
-	private char dir = 'u';
-	
-	BufferedImage shield;
-	
-	int red = 30;
-
-	public Player() {
-	}
-	
-	public void shield(char direction, Graphics g){
-		this.dir = direction;
-		shieldDir();
-		drawShield(g);
-	}
-	
-	public void drawShield(Graphics g) {
-		try {
-			if (red < 25) {
-				shield = ImageIO.read(new File("shieldh.png"));
-				++red;
-			} else {
-				shield = ImageIO.read(new File("shield.png"));
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		g.translate(300, 300);
-		AffineTransform tx = new AffineTransform();
-		tx.rotate(Math.toRadians(angle), shield.getMinX() + shield.getWidth() / 2,
-				shield.getMinY() + shield.getHeight() / 2);
-		AffineTransformOp op = new AffineTransformOp(tx, AffineTransformOp.TYPE_BILINEAR);
-		shield = op.filter(shield, null);
-		g.translate(-300, -300);
-		g.drawImage(shield, 265, 254, null);
-	}
-
-	public void shieldDir() {
-		switch (dir) {
-		case 'r':
-			if (angle == 90)
-				break;
-			else if (angle > 90 && angle <= 180)
-				angle -= 15;
-			else
-				angle += 15;
-			break;
-		case 'l':
-			if (angle == 270)
-				break;
-			else if (angle < 270 && angle >= 180)
-				angle += 15;
-			else if (angle >= 0)
-				angle -= 15;
-			if (angle < 0)
-				angle = 360 + angle;
-			break;
-		case 'u':
-			if (angle == 0)
-				break;
-			else if (angle <= 90 && angle > 0)
-				angle -= 15;
-			else
-				angle += 15;
-			break;
-		case 'd':
-			if (angle == 180)
-				break;
-			else if (angle <= 270 && angle > 180)
-				angle -= 15;
-			else if (angle <= 360 && angle > 270)
-				angle -= 15;
-			else
-				angle += 15;
-			break;
-		}
-		if (angle > 360)
-			angle = 0;
-	}
-
-	public void damage() {
-		playerHealth -= (int) (Math.random() * 9 + 14);
-	}
-	
-	public void setRed(int red){
-		this.red = red;
-	}
-	
-	public char getDir(){
-		return dir;
-	}
-
+    /*
+     * Delay in milliseconds between each movement of the player
+     */
+    private double playerDelay = 0;
+    /*
+     * Initial player health
+     */
+    private int playerHealth = 100;
+    /*
+     * How much the player is currently rotated by
+     */
+    private int rotated = 180;
+    
+    public Player() {}
+    
+    public void right() {
+        
+    }
+    
+    public void left() {
+        
+    }
+    
+    public void up() {
+        
+    }
+    
+    public void down() {
+        
+    }
+    
+    public void damage() {
+        playerHealth -= (int) (Math.random() * 9 + 14);
+    }
+    
 }
