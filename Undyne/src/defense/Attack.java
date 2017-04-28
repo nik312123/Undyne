bd5d70a39dcf831c6a24ff82544d2bd4ea8a9f19
akandikatla <aaron4game@gmail.com>
2017-04-27 19:18:44 -0500

    Screen shake

--- Undyne/src/defense/Attack.java
+++ Undyne/src/defense/Attack.java
@@ -9,11 +9,10 @@ import javax.sound.sampled.UnsupportedAudioFileException;
 
 import nikunj.classes.Sound;
 
-
 public class Attack {
-	public static int currentDirection = 0;
-	public static final char[] DIRS = { 'u', 'd', 'r', 'l' };
-	private int counter = 49;
+    public static int currentDirection = 0;
+    public static final char[] DIRS = { 'u', 'd', 'r', 'l' };
+    private int counter = 0;
     /*
      * List of Arrow objects that make up one attack
      */
@@ -26,145 +25,155 @@ public class Attack {
      * ifthe delays vary in amount for the arrows, this is it (milliseconds)
      */
     private double[] delayGroup;
-    
+
     public static String hit = "";
-    
+
+    boolean isDamaged = false;
+    int move = 0;
+    Player p;
+
     /*
      * 
      * Constructor for constant delay
      */
-    public Attack(LinkedList<Arrow> attackPattern, double delay) {
+    public Attack(LinkedList<Arrow> attackPattern, double delay, Player p) {
         this.attackPattern = attackPattern;
         this.delay = delay;
         this.delayGroup = new double[0];
+        this.p = p;
     }
-    
+
     /*
      * Constructor for varying delays
      */
     public Attack(LinkedList<Arrow> attackPattern, double[] delayGroup) throws IOException {
-        if(delayGroup.length == attackPattern.size() - 1)
+        if (delayGroup.length == attackPattern.size() - 1)
             throw new IOException("Error: Number of delays must be one less than the number of attacks");
         this.attackPattern = attackPattern;
         this.delay = 0;
         this.delayGroup = delayGroup;
     }
-    
+
     public void tick() {
-        for(Arrow a : attackPattern)
+        for (Arrow a : attackPattern)
             a.tick();
     }
-    
+
     public void addArrow(Arrow a) {
         attackPattern.add(a);
     }
-    
+
     public void removeArrow(Arrow a) {
         attackPattern.remove(a);
     }
-    
+
     public String removeArrow(char dir, Player p) {
         boolean hit = false;
         boolean damage = false;
-        if(attackPattern.size() == 0)
+        if (attackPattern.size() == 0)
             return "";
         for (int i = 0; i < attackPattern.size(); i++) {
             Arrow tempArrow = attackPattern.get(i);
-            switch(tempArrow.getDir()) {
-                case 'l':
-                    if(dir == 'r') {
-                        if(tempArrow.getX() < 325) {
-                            attackPattern.remove(i);
-                            hit = true;
-                        }
-                    }
-                    else if(attackPattern.get(i).getX() < 308) {
+            switch (tempArrow.getDir()) {
+            case 'l':
+                if (dir == 'r') {
+                    if (tempArrow.getX() < 325) {
                         attackPattern.remove(i);
-                        damage = true;
+                        hit = true;
                     }
-                    break;
-                case 'r':
-                    if(dir == 'l') {
-                        if(tempArrow.getX() > 240) {
-                            attackPattern.remove(i);
-                            hit = true;
-                        }
-                    }
-                    else if(attackPattern.get(i).getX() > 261) {
+                } else if (attackPattern.get(i).getX() < 308) {
+                    attackPattern.remove(i);
+                    damage = true;
+                }
+                break;
+            case 'r':
+                if (dir == 'l') {
+                    if (tempArrow.getX() > 240) {
                         attackPattern.remove(i);
-                        damage = true;
+                        hit = true;
                     }
-                    break;
-                case 'u':
-                    if(dir == 'd') {
-                        if(tempArrow.getY() < 320) {
-                            attackPattern.remove(i);
-                            hit = true;
-                        }
-                    }
-                    else if(attackPattern.get(i).getY() < 295) {
+                } else if (attackPattern.get(i).getX() > 261) {
+                    attackPattern.remove(i);
+                    damage = true;
+                }
+                break;
+            case 'u':
+                if (dir == 'd') {
+                    if (tempArrow.getY() < 320) {
                         attackPattern.remove(i);
-                        damage = true;
+                        hit = true;
                     }
-                    break;
-                case 'd':
-                    if(dir == 'u') {
-                        if(attackPattern.get(i).getY() > 230) {
-                            attackPattern.remove(i);
-                            hit = true;
-                        }
-                    }
-                    else if(attackPattern.get(i).getY() > 252) {
+                } else if (attackPattern.get(i).getY() < 295) {
+                    attackPattern.remove(i);
+                    damage = true;
+                }
+                break;
+            case 'd':
+                if (dir == 'u') {
+                    if (attackPattern.get(i).getY() > 230) {
                         attackPattern.remove(i);
-                        damage = true;
+                        hit = true;
                     }
-                    break;
+                } else if (attackPattern.get(i).getY() > 252) {
+                    attackPattern.remove(i);
+                    damage = true;
+                }
+                break;
             }
         }
-        if(hit)
+        if (hit)
             return "H";
-        else if(damage){
+        else if (damage) {
             p.damage();
             return "D";
         }
         return "";
     }
-    
+
     public void draw(Graphics g) throws IOException {
-        if(attackPattern.size() == 0)
+        if (attackPattern.size() == 0)
             return;
         attackPattern.get(0).draw(g, Color.RED);
-        for(int i = 1; i < attackPattern.size(); ++i) {
+        for (int i = 1; i < attackPattern.size(); ++i) {
             attackPattern.get(i).draw(g, Color.BLUE);
         }
     }
-    
+
     public void spawnArrows(Graphics g, Player p) throws IOException {
-	    tick();
-	    if(++counter == 50) {
-	        addArrow(new Arrow(2, false, DIRS[currentDirection++]));
-	        if(currentDirection == DIRS.length)
-	            currentDirection = 0;
-	        counter = 0;
-	    }
-	    hit = removeArrow(p.getDir(),p);
-	    Sound block = null;
-	    Sound damage = null;
+
+        tick();
+        if (++counter == 40) {
+            addArrow(new Arrow(1, false, DIRS[currentDirection++], p));
+            if (currentDirection == DIRS.length)
+                currentDirection = 0;
+            counter = 0;
+        }
+        hit = removeArrow(p.getDir(), p);
+        Sound block = null;
+        Sound damage = null;
         try {
             block = new Sound("Audio/block.wav", false);
             damage = new Sound("Audio/damage.wav", false);
         } catch (IOException | UnsupportedAudioFileException e) {
             e.printStackTrace();
         }
-	    if(hit.equals("H")) {
-	        p.setRed(0);
-	        block.play();
-	    }
-	    else if(hit.equals("D"))
-	        damage.play();
-	    draw(g);
-	}
-    
-    
-    
+        if (hit.equals("H")) {
+            p.setRed(0);
+            block.play();
+        } else if (hit.equals("D")) {
+            isDamaged = true;
+            damage.play();
+            hit = "";
+
+        }
+        if (isDamaged) { 
+            p.setElementPosition(move += 2);
+            if (move >= 5) {
+                isDamaged = false;
+                move = 0;
+                p.setElementPosition(10); 
+            }
+        }
+        draw(g); 
+    }
 }
